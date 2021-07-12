63365cadbdac6b6698e832882e81b3d8ac17fec8
koskos1986 <ekosopletkin@yandex.ru>
2021-07-12 22:55:21 +0300

    add PP for product-card

--- build/js/main.js
+++ build/js/main.js
@@ -274,41 +274,6 @@
 })();
 
 (function () {
-/*const accordion = () => {
-    const pageClass = {
-      ACCORDION_CLASS: 'js-btn-accordion',
-      ACTIVE_CLASS: '--active',
-    };
-
-    const accordionBlock = document.querySelector('.js-accordion');
-    const accordionBtn = document.querySelectorAll('.js-btn-accordion');
-    const accordionItems = document.querySelectorAll('.js-accordion-item');
-    accordionBlock.classList.remove('js-accordion--nojs');
-
-    const toggleAccordionHandler = (evt) => {
-      const target = evt.target.closest('li');
-      const activeClass = `${target.dataset.class}${pageClass.ACTIVE_CLASS}`;
-      if (target.classList.contains(activeClass)) {
-        target.classList.remove(activeClass);
-      } else {
-        for (let item of accordionItems) {
-          if (item.classList.contains(activeClass)) {
-            item.classList.remove(activeClass);
-          }
-        }
-        target.classList.add(activeClass);
-      }
-    };
-
-    if (accordionBlock) {
-      for (let btn of accordionBtn) {
-        btn.addEventListener('click', toggleAccordionHandler);
-      }
-    }
-  };
-  accordion();
-  */
-
   const acc = document.getElementsByClassName("faq__btn");
   let i;
 
